8247d3de122ad740bd06c827b8e6268371e98bd1
Arvind Manivannan <arvindmanivannan993@gmail.com>
2024-07-15 05:43:51 +0530

    Included contents in groups app html templates

--- social_clone/groups/views.py
+++ social_clone/groups/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from .models import Group, GroupMemeber
 # Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.urls import reverse
+from django.views import generic
+
+class CreateGroup(LoginRequiredMixin, generic.CreateView):
+    fields = ('name', 'description')
+    model = Group
+
+class SingleGroup(generic.DetailView):
+    model = Group
+
+class ListGroups(generic.ListView):
+    model = Group
